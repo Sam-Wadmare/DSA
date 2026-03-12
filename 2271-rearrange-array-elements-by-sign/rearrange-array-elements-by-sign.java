17b9fe91ea5fb738f904a17d6a773ab66f0f6032
Sam-Wadmare <159400424+Sam-Wadmare@users.noreply.github.com>
2026-03-12 21:18:14 +0530

    Time: 3 ms (99.64%) | Memory: 175.5 MB (66.91%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,41 +1,20 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
-        int[] result = new int[n];
-        int posIndex = 0; 
-        int negIndex = 1;
-
-        for(int num : nums) {
-            if(num > 0){
-                result[posIndex] = num;
-                posIndex+=2;    
-            }else {
-                result[negIndex]=num;
-                negIndex+=2;
+        int[] ans = new int[n];
+        int pos = 0;
+        int neg = 1;
+        for(int i=0;i<n;i++){
+            if(nums[i] > 0){
+                ans[pos] = nums[i];
+                pos +=2;
+            }
+            else{
+                ans[neg] = nums[i];
+                neg +=2;
             }
         }
-        return result;
+
+        return ans;
     }
 }
-// class Solution {
-//     public int[] rearrangeArray(int[] nums) {
-
-//         int n = nums.length;
-//         int[] result = new int[n];
-
-//         int posIndex = 0; // index for positive numbers
-//         int negIndex = 1; // index for negative numbers
-
-//         for (int num : nums) {
-//             if (num > 0) {
-//                 result[posIndex] = num;
-//                 posIndex += 2;
-//             } else {
-//                 result[negIndex] = num;
-//                 negIndex += 2;
-//             }
-//         }
-
-//         return result;
-//     }
-// }
