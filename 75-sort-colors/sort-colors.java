a0912a9dab33ddc0ca3cac197f3dcffb5f8543f9
Samyak Wadmare <159400424+Sam-Wadmare@users.noreply.github.com>
2026-07-28 23:48:44 +0530

    Time: 3 ms (14.86%) | Memory: 43.7 MB (25.51%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,35 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-
-        int count0=0;
-        int count1=0;
-        int count2=0;
-
-        for(int count: nums){
-            if (count == 0){
-                count0++;
-            }
-            else if(count == 1){
-                count1++;
-            }
-            else{
-                count2++;
-            }
-        }
-
-        int index = 0;
-        while(count0-- > 0){
-            nums[index++]=0;
-        }
-        while(count1-- > 0){
-            nums[index++]=1;
-        }
-        while(count2-- > 0){
-            nums[index++]=2;
-        }
-        for(int num : nums){
-            System.out.println(num);
-        }
+        Arrays.sort(nums);
     }
 }
