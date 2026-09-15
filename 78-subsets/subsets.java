7ad95abc4a1e8b3a31ae5c8cb900bcffab56ab0a
Samyak Wadmare <159400424+Sam-Wadmare@users.noreply.github.com>
2026-09-16 01:08:52 +0600

    Time: 1 ms (87.24%) | Memory: 44.3 MB (20.23%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        // Start with empty subset
+        ans.add(new ArrayList<>());
+
+        for (int num : nums) {
+
+            int size = ans.size();
+
+            for (int i = 0; i < size; i++) {
+
+                List<Integer> newSubset = new ArrayList<>(ans.get(i));
+
+                newSubset.add(num);
+
+                ans.add(newSubset);
+            }
+        }
+
+        return ans;
+    }
+}
